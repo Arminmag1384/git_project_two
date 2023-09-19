da106a015f894bf63f472ab7805ec7bf4efe4fa9
armin.mag <armin84maghsudi@gmail.com>
2023-09-19 19:39:13 +0330

    ubdated word

--- incedis.py
+++ incedis.py
@@ -1,9 +1,9 @@
 y = input("what is name?")
 print(f"Hello {y}")
 for x in range(0,1000):
-    print(f"{x}: hi")
+    print(f"{x}: Hi")
     if x >= 10:
         print("tob")
     if x >= 1000:
-        print(f"\t{x} : bye")
-print("good bye")
+        print(f"\t{x} : bye $")
+print("Good Bye")
